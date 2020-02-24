3614df0f92c260ccd0a48e86d9a6b566ffa314f9
Jason <jasonjasyno50@gmail.com>
2020-02-24 15:19:16 +0100

    updated footer styles

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,6 +1,6 @@
 // Package import(s)
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 // Component import(s)
 
 
@@ -9,7 +9,43 @@ import './footer.styles.scss'
 
 const Footer = () => (
     <footer>
-        
+        <div className="content-wrapper">
+            <div className="column">
+                <h3>Doctors</h3>
+                <Link>lorem</Link>
+                <Link>lorem</Link>
+                <Link>lorem</Link>
+                <Link>lorem</Link>
+                <Link>lorem</Link>
+            </div>
+
+            <div className="column">
+                <h3>Doctors</h3>
+                <Link>lorem</Link>
+                <Link>lorem</Link>
+                <Link>lorem</Link>
+                <Link>lorem</Link>
+                <Link>lorem</Link>
+            </div>
+
+            <div className="column">
+                <h3>Doctors</h3>
+                <Link>lorem</Link>
+                <Link>lorem</Link>
+                <Link>lorem</Link>
+                <Link>lorem</Link>
+                <Link>lorem</Link>
+            </div>
+            
+            <div className="column">
+                <h3>Doctors</h3>
+                <Link>lorem</Link>
+                <Link>lorem</Link>
+                <Link>lorem</Link>
+                <Link>lorem</Link>
+                <Link>lorem</Link>
+            </div>
+        </div>
     </footer>
 )
 
--- src/pages/doctors/doctors.page.jsx
+++ src/pages/doctors/doctors.page.jsx
@@ -2,8 +2,26 @@ import React from 'react'
 
 import './doctors.styles.scss'
 
-const DoctorsPage = () => (
-    <div className="doctors"></div>
-)
+const DoctorsPage = () => {
+    const stu = document.querySelector("#dashboard-wrapper")
+    // const stuff = stu.querySelectorAll('nav > ul > li')
+    console.log(stu)
 
+    return (
+        <div className="doctors">
+            <div className="dashboard-wrapper">
+                <nav>
+                    <ul>
+                        <li>Bread</li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </nav>
+            </div>
+
+            <div className="dashboard-display"></div>
+        </div>
+    )
+}
 export default DoctorsPage;
