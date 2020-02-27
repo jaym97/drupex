33e4fc21ddaccc20b5ed91c0ee1957aafbc0b5a4
Jason <jasonjasyno50@gmail.com>
2020-02-27 11:56:30 +0100

    making further improvements to the footer styles and content

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,49 +1,45 @@
 // Package import(s)
 import React from 'react'
 import { Link } from 'react-router-dom'
-// Component import(s)
 
+// Icon import(s)
+import FacebookIcon from '../../assets/svg-components/facebook.icon'
+import LinkedInIcon from '../../assets/svg-components/linkedin.icon'
+import TwitterIcon from '../../assets/svg-components/twitter.icon'
 
 // Style import(s)
 import './footer.styles.scss'
 
 const Footer = () => (
     <footer>
-        <div className="content-wrapper">
+        <div className="main-links">
             <div className="column">
-                <h3>Doctors</h3>
-                <Link>lorem</Link>
-                <Link>lorem</Link>
-                <Link>lorem</Link>
-                <Link>lorem</Link>
-                <Link>lorem</Link>
+                <Link to="/about-us">About us</Link>
+                <Link to="/think-tank">How it works</Link>
+                <Link to="/patients">Get a prescription</Link>
+                <Link to="/prescription-requests">Make a prescription</Link>
             </div>
 
             <div className="column">
-                <h3>Doctors</h3>
-                <Link>lorem</Link>
-                <Link>lorem</Link>
-                <Link>lorem</Link>
-                <Link>lorem</Link>
-                <Link>lorem</Link>
+                <Link to="/contact-us">Contact us</Link>
+                <Link to="/help">FAQ & Help</Link>
             </div>
+        </div>
 
-            <div className="column">
-                <h3>Doctors</h3>
-                <Link>lorem</Link>
-                <Link>lorem</Link>
-                <Link>lorem</Link>
-                <Link>lorem</Link>
-                <Link>lorem</Link>
+        <div className="other-links">
+            <div className="social-links">
+                <a href="https://www.facebook.com/" target="_blank">
+                    <FacebookIcon id="click here to go to our (non-existent :D) facebook page" />
+                </a>
+                <a href="https://www.linkedin.com/in/jason-omemu" target="_blank">
+                    <LinkedInIcon id="click here to reach out on LinkedIn" />
+                </a>
+                <a href="https://www.linkedin.com/in/jason-omemu" target="_blank">
+                    <TwitterIcon id="click here to reach out on Twitter" />
+                </a>
             </div>
-            
-            <div className="column">
-                <h3>Doctors</h3>
-                <Link>lorem</Link>
-                <Link>lorem</Link>
-                <Link>lorem</Link>
-                <Link>lorem</Link>
-                <Link>lorem</Link>
+            <div className="other-details">
+                <Link to="/icon-credits">Icon credits</Link>
             </div>
         </div>
     </footer>
