e6cf41a7941dd0fbce5ad37d9e73cdd19394155f
Jason <jasonjasyno50@gmail.com>
2020-03-18 12:01:36 +0100

    improved modal styles and responsiveness of header component

--- src/components/modal/modal.component.jsx
+++ src/components/modal/modal.component.jsx
@@ -5,13 +5,13 @@ import CustomButton from '../custom-button/custom-button.component'
 import './modal.styles.scss'
 
 const Modal = ({ isOpen, handleClose, children }) => {
-    const showHideClassName = isOpen ? "modal visible-display" : "modal display-none";
-    console.log(isOpen,3)
+  const showHideClassName = isOpen ? "modal visible-display" : "modal display-none"
+  console.log(isOpen, 5)
   return (
     <div className={showHideClassName}>
       <section className="modal-main">
-          {children}
-          <CustomButton primary onClick={handleClose}>close</CustomButton>
+        <div className="modal-main_header"><CustomButton primary onClick={handleClose}>&times;</CustomButton></div>
+        {children}
       </section>
     </div>
   )
