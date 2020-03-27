9a48a4c83950c200eb8cb9b8522963034df1f853
Jason <jasonjasyno50@gmail.com>
2020-03-27 14:02:26 +0100

    added icons to dashboard links

--- src/components/dashboard/dashboard.component.jsx
+++ src/components/dashboard/dashboard.component.jsx
@@ -4,8 +4,15 @@ import { Link, Switch, Route } from 'react-router-dom'
 import DashboardChart from '../dashboard-chart/dashboard-chart.component'
 import ImageUpload from '../image-upload/image-upload.component'
 
-import './dashboard.styles.scss'
 import PrescriptionRequests from '../../pages/prescription-requests/prescription-requests.page'
+
+import CogsIcon from '../../assets/svg-components/cogs.icon'
+import ConfirmedIcon from '../../assets/svg-components/confirmed.icon'
+import DashboardIcon from '../../assets/svg-components/dashboard.icon'
+import EditProfileIcon from '../../assets/svg-components/edit-profile.icon'
+import MultipleMailIcon from '../../assets/svg-components/multiple-mail.icon'
+
+import './dashboard.styles.scss'
 
 class Dashboard extends Component {
     state = {
@@ -20,10 +27,26 @@ class Dashboard extends Component {
                    <nav>
                        <ul>
                             <li id="profile-img"><ImageUpload /></li>
-                           <li><Link to="/doctors/prescription-requests">Prescription Requests</Link></li>
-                           <li><Link to="/edit-profile">Edit Profile</Link></li>
-                           <li><Link to="/manage-account">Manage Account</Link></li>
-                           <li><Link to="/manage-account">Handled Requests</Link></li>
+                            <li>
+                                <DashboardIcon />
+                                <Link to="/doctors">Dashboard</Link>
+                            </li>
+                            <li>
+                                <MultipleMailIcon id="unread" title="Check your assigned prescription request" />
+                                <Link to="/doctors/prescription-requests">Prescription Requests</Link>
+                            </li>
+                            <li>
+                                <EditProfileIcon />
+                                <Link to="/edit-profile">Edit Profile</Link>
+                            </li>
+                            <li>
+                                <CogsIcon />
+                                <Link to="/manage-account">Manage Your Account</Link>
+                            </li>
+                            <li>
+                                <ConfirmedIcon />
+                                <Link to="/manage-account">Handled Requests</Link>
+                            </li>
                        </ul>
                    </nav>
                </div>
